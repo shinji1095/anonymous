0540fac07d3a54327c46f4342c6b95ad20c4318d
tknr311 <71833564+tknr311@users.noreply.github.com>
2021-07-23 23:23:09 +0900

    add:card機能の追加

--- front/my-app/components/card.tsx
+++ front/my-app/components/card.tsx
@@ -8,8 +8,8 @@ export const Card: FC<{assign:any, date:any}>
             <div className="card border-left-primary shadow h-100 py-2">
                 <div className="card-body">
                     <div className="row no-gutters align-items-center">
-                        <div className="col mr-2">
-                            <div className="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                        <div className="col mr-12">
+                            <div className="text-xs3 font-weight-bold text-primary text-uppercase mb-1">
                                 Deadline:  {date}</div>
                                     {assign.map((data:any)=>
                                         <CardSwitch assignment={data}/>
--- front/my-app/components/cardSwitch.tsx
+++ front/my-app/components/cardSwitch.tsx
@@ -6,26 +6,28 @@ export const CardSwitch: FC<{assignment:any}>
                     = ({assignment}) =>{
     return (
             // <div className="h5 mb-0 font-weight-bold text-gray-800">
-            <div className="h5 xs-4">
+            // <div className="h5 xs-4">
                 <div className="container">
                     <div className="row">
                         {/* <div className=" col-xs-2 text-xs font-weight-bold text-primary text-uppercase mb-1"> */}
-                        <div className="h5 mb-0 font-weight-bold text-gray-800">
+                        {/* <div className="h5 mb-0 font-weight-bold w-50 text-gray-800"> */}
+                        <div className="small col-md-6 font-weight-bold text-gray-800">
                             {assignment}
                         </div>
-                        <div className="col-xs-3">
-                            <button type="button" className="btn btn-success text-white">
+                        {/* </div> */}
+                        {/* <div className="col-xs-3"> */}
+                            <button type="button" className="btn btn-success col-md-3 text-white">
                                 <YaruyoButton assName={assignment}/>
                             </button>
-                        </div>
-                        <div className="col-xs-3">
-                            <button type="button" className="btn btn-danger text-white">
+                        {/* </div> */}
+                        {/* <div className="col-xs-3"> */}
+                            <button type="button" className="btn btn-danger col-md-3 text-white">
                                 <OwataButton assName={assignment}/>
                             </button>
-                        </div>
+                        {/* </div> */}
                     </div>
                 </div>
-            </div>
+            // </div>
     )
 }
 
