f62b6e8d210a401ceb05c2ff3c07729b1a79aca9
tknr311 <71833564+tknr311@users.noreply.github.com>
2021-07-25 12:44:05 +0900

    fix/card:ボタン配置の変更

--- front/my-app/components/cardSwitch.tsx
+++ front/my-app/components/cardSwitch.tsx
@@ -6,15 +6,15 @@ import Assignment from '../type/assignment'
 export const CardSwitch: FC<{assignment:Assignment}> 
                     = ({assignment}) =>{
     return (
-            <div>
+            <div className="mt-1">
                 <div className="h5 xs-0 font-weight-bold text-gray-800">
                     {assignment.name}
                 </div>
-                <div className="row justify-content-around">
-                    <button type="button" className="btn-sm btn-success text-white">
+                <div className="row justify-content-center">
+                    <button type="button" className="mr-2 btn-sm btn-success text-white">
                         <YaruyoButton assignment={assignment}/>
                     </button>
-                    <button type="button" className="btn-sm btn-danger text-white">
+                    <button type="button" className="btn-sm btn-danger text-white ml-2">
                         <OwataButton assignment={assignment}/>
                     </button>
                 </div>
